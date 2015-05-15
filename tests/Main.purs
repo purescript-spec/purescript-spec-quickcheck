03479e4229783fc3553f9bd13bf228fb472d3cc7
Oskar Wickström <oskar.wickstrom@live.com>
2015-05-15 15:43:24 +0200

    Upgrade to purescript-spec 0.5.0

--- tests/Main.purs
+++ tests/Main.purs
@@ -5,9 +5,10 @@ import Test.QuickCheck ((===), (/==))
 import Test.Spec (describe, pending, it)
 import Test.Spec.Node
 import Test.Spec.Assertions
+import Test.Spec.Reporter.Console
 import Test.Spec.QuickCheck
 
-main = runNode do
+main = runNode [consoleReporter] do
   describe "Math" do
     it "works" $
       quickCheck \n -> (n * 2 / 2) === n
