137aa848022dba49c1cbe5a72c254f2ec646baf0
David Harrison <djrh777@gmail.com>
2015-11-14 17:36:56 -0800

    Remove warnings.

--- src/Test/Spec/QuickCheck.purs
+++ src/Test/Spec/QuickCheck.purs
@@ -18,14 +18,14 @@ import qualified Test.QuickCheck   as QC
 import Test.QuickCheck.LCG         (Seed(), randomSeed)
 
 -- | Runs a Testable with a random seed and 100 inputs.
-quickCheck :: forall r p e.
+quickCheck :: forall p e.
               (QC.Testable p) =>
               p ->
               Aff (random :: RANDOM | e) Unit
 quickCheck = quickCheck' 100
 
 -- | Runs a Testable with a random seed and the given number of inputs.
-quickCheck' :: forall r p e.
+quickCheck' :: forall p e.
                (QC.Testable p) =>
                Int ->
                p ->
@@ -39,7 +39,7 @@ getErrorMessage (QC.Failed msg) = Just msg
 getErrorMessage _ = Nothing
 
 -- | Runs a Testable with a given seed and number of inputs.
-quickCheckPure :: forall r p e.
+quickCheckPure :: forall p e.
                   (QC.Testable p) =>
                   Seed ->
                   Int ->
