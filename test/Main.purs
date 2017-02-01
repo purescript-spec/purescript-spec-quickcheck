8a935697e42bc2fede2d640f607446f664c80411
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-02-01 21:00:00 +0100

    Adapt to purescript-spec 0.12.x

--- test/Main.purs
+++ test/Main.purs
@@ -1,17 +1,15 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Aff.Console (CONSOLE)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Random (RANDOM)
-import Node.Process (PROCESS)
 import Test.QuickCheck ((===), (/==))
 import Test.Spec (describe, it)
 import Test.Spec.QuickCheck (quickCheck)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (run)
+import Test.Spec.Runner (RunnerEffects, run)
 
-main :: Eff (process :: PROCESS, console :: CONSOLE, random :: RANDOM) Unit
+main :: Eff (RunnerEffects (random :: RANDOM)) Unit
 main = run [consoleReporter] do
   describe "Math" do
     it "works" $
