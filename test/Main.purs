c2991f475b8fa11de8b68bcb5895b36be04d1e82
Fyodor Soikin <name.fa@gmail.com>
2021-03-02 18:28:03 -0500

    Migrate to PureScript 0.14.0 (#15)

--- test/Main.purs
+++ test/Main.purs
@@ -1,15 +1,17 @@
 module Test.Main where
 
 import Prelude
+
 import Effect (Effect)
+import Effect.Aff (launchAff_)
 import Test.QuickCheck ((===), (/==))
 import Test.Spec (describe, it)
 import Test.Spec.QuickCheck (quickCheck)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (run)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = run [consoleReporter] do
+main = launchAff_ $ runSpec [consoleReporter] do
   describe "Math" do
     it "works" $
       quickCheck \n -> (n * 2 / 2) === n
